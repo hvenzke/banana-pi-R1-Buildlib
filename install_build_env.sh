e985b5bda47f180043d15c2addd55b1bb437da9d
Horst Venzke <hvenzke@users.noreply.github.com>
2015-03-19 20:17:35 +0100

    Create install_build_env.sh
    script for sufficient linux kernel compiling envirement

---
+++ install_build_env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+apt-get -y install apt-file less vim 
+apt-get -y install devscripts equivs wget build-essential fakeroot
+apt-get -y  install automake autoconf bison flex libtool  
+apt-get -y -qq install git
+apt-get -y install libncurses-dev ncurses-dev 
