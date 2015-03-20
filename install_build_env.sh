2b7890d23a051aad3cbd876f51f294cd5882304f
Horst Venzke <hvenzke@users.noreply.github.com>
2015-03-20 10:11:57 +0100

    Update install_build_env.sh

--- install_build_env.sh
+++ install_build_env.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # v 0.2
+
+# docu refs:
 # https://wiki.debian.org/HowToRebuildAnOfficialDebianKernelPackage
+# http://wiki.oppserver.net/index.php/DEBIAN_KERNEL_BAUEN
 
 # script for setup debian kernel build and packaging
 
