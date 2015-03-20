dfcc25f995de196b080df66578fad0553e7ea9f2
Horst Venzke <hvenzke@users.noreply.github.com>
2015-03-20 09:00:07 +0100

    Update install_build_env.sh
    add  make-kpkg for createing .deb from kernel sources.

--- install_build_env.sh
+++ install_build_env.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+# v 0.2
+# https://wiki.debian.org/HowToRebuildAnOfficialDebianKernelPackage
+
+# script for setup debian kernel build and packaging
 
 apt-get -y install apt-file less vim 
-apt-get -y install devscripts equivs wget build-essential fakeroot
+apt-get -y install devscripts equivs wget build-essential fakeroot  make-kpkg
 apt-get -y  install automake autoconf bison flex libtool  
 apt-get -y -qq install git
 apt-get -y install libncurses-dev ncurses-dev 
